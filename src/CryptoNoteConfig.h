5e872a24f0d590d2cf3c88541d827a9f0cfc0a16
Ubuntu <traveldinero@ubuntu2.zbcvpyoftxhuddrdf3p005cnah.fx.internal.cloudapp.net>
2017-09-14 12:22:32 +0000

    seeds

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
+  "40.113.87.44:17236",
   "52.169.126.44:17236"
-  //"your_seed_ip2.com:8080",
 };
 
 struct CheckpointData {
